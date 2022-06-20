81561dc00b2841b433f1f47d27b320c8c61fe563
Llaauuttyy <ldabbraccio@fi.uba.ar>
2022-06-20 00:31:24 -0300

    Se agrega clase ProductsScrapper

---
+++ utils/scrapper/ProductsScrapper.rb
@@ -0,0 +1,24 @@
+require "nokogiri"
+
+PRICES_XPATH = "//span[@class='atg_store_productPrice'][not(contains(@style, 'visibility: hidden'))] | //div[@class='first_price_discount_container']/span[@class='price_discount']"
+TITLES_XPATH = "//div[@class='descrip_full']"
+ICONS_XPATH = "//span[@class='atg_store_productImage']/img"
+
+class ProductsScrapper
+    def scrap(page_to_scrap)
+        return [scrap_titles(page_to_scrap), scrap_prices(page_to_scrap), scrap_icons(page_to_scrap)]
+    end
+
+    # Unificar en 1
+    def scrap_titles(page_to_scrap)
+        titles = page_to_scrap.xpath(TITLES_XPATH).text
+    end
+
+    def scrap_prices(page_to_scrap)
+        prices = page_to_scrap.xpath(PRICES_XPATH).text
+    end
+
+    def scrap_icons(page_to_scrap)
+        icons = page_to_scrap.xpath(ICONS_XPATH).text
+    end
+end
