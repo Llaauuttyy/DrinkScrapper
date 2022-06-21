23b77ac72841fd4d488fe84512351935e3ad6c09
Llaauuttyy <ldabbraccio@fi.uba.ar>
2022-06-20 21:49:09 -0300

    Agrego ruta con params para botones

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   get '', to: "products#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get "/index", to: "products#index"
+
+  post "/index/:section_name", to: "products#button"
+
   get "/products", to: "products#index"
   get "/drinks_with_alcohol", to: "products#drinks_with_alcohol"
   get "/drinks_without_alcohol", to: "products#drinks_without_alcohol"
